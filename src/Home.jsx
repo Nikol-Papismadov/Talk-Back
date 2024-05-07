ef6979e752649f481cf7ea8a192627287e383456
nitzan-nagar <nitzan.nagar@gmail.com>
2024-05-07 22:28:23 +0300

    navigation

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
 import Game from './componenets/game/Game';
-import Logout from './componenets/authentication/Logout';
 import OnlineUsers from './componenets/onlineUsers/OnlineUsers';
+import Navigation from './componenets/layout/Navigation';
 
 const Home = () => {
   const user = sessionStorage.getItem("username");
   return (
     <div>
-      <p>Logged in as: {user}</p>
+      <Navigation></Navigation>
       <Game></Game> 
       <OnlineUsers/>
-      <Logout></Logout>
     </div>
   );
 };
---
+++ src/componenets/layout/Navigation.jsx
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import './navigation.css'
+import userIcon from '../../assets/user-icon.png'
+
+const Navigation = () => {
+    const user = sessionStorage.getItem("username");
+    const [MenuVisibility, setMenuVisibility] = useState('hidden');
+    const handleMenuVisibility = () => {
+        MenuVisibility === 'hidden' ? setMenuVisibility('visible') : setMenuVisibility('hidden');
+    }
+  return (
+    <div className='navigationBar'>
+        <h2>TalkBack</h2>
+        <p>Hello {user}</p>
+        <img className='user-icon' src={userIcon} alt="user-icon" />
+        <button onClick={handleMenuVisibility}>Menu</button>
+        <div style={{display: {MenuVisibility}}}>
+            <ul>
+                <li>Chat Messages</li>
+                <li>Game Requests</li>
+            </ul>
+            <button>Logout</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navigation
