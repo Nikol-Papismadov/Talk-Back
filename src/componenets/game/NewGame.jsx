c3c29b42cfa9b888c0da30d3b0758a5b5b799feb
nitzan-nagar <nitzan.nagar@gmail.com>
2024-05-09 01:09:47 +0300

    fix

--- src/componenets/game/NewGame.jsx
+++ src/componenets/game/NewGame.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
-import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+import { WINNING_COMBINATIONS } from "./winning_combinations.js";
 import GameOver from "./components/GameOver.jsx";
 
 import React from "react";
